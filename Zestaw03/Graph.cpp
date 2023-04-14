ba369dfdb253dcb8c73d647e1e75221ee0f9c5d7
Dawid Włosek <dawid.wllosek@gmail.com>
2023-04-14 19:22:25 +0200

    Szybkie sprawdzenie metod

--- Zestaw03/Graph.cpp
+++ Zestaw03/Graph.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include "Graph.hpp"
+#include "Set.hpp"
 
 auto main() -> int{
     Graph myGraph(1000);
     myGraph.addEdge(4, 5);
-    myGraph.addEdge(4, 3);
-    myGraph.addEdge(3, 1);
     myGraph.addEdge(4, 1);
-    myGraph.addEdge(5, 7);
-    myGraph.addEdge(7, 1);
-    myGraph.addEdge(3, 9);
-    myGraph.addEdge(9, 5);
+    myGraph.addEdge(4, 0);
+    myGraph.addEdge(4, 2);
+    myGraph.addEdge(5, 1);
+    myGraph.addEdge(5, 0);
+    myGraph.addEdge(0, 2);
+    myGraph.addEdge(2,3);
     int * distance = myGraph.BFS(myGraph, 5);
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < myGraph.size(); i++){
         std::cout << distance[i] << std::endl;
     }
+    Set<int> mySet(1000);
+    mySet = myGraph.outConnections(4);
+    mySet.displaySet();
+
+    if(myGraph.hasEdge(0,5))
+        std::cout << "Yeah" << std::endl;
+    myGraph.removeEdge(0, 5);
+    if(myGraph.hasEdge(0,5))
+        std::cout << "Yeah2" << std::endl;
 }
