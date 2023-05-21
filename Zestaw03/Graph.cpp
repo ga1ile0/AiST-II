040209627ba4ac40ac601fad6302a6f006d4a152
Dawid Włosek <dawid.wllosek@gmail.com>
2023-05-22 00:31:03 +0200

    Zestaw03 skończony.

--- Zestaw03/Graph.cpp
+++ Zestaw03/Graph.cpp
@@ -5,26 +5,24 @@
 auto main() -> int{
     Graph myGraph(1000);
     myGraph.addEdge(4, 5);
+
     myGraph.addEdge(4, 1);
+
     myGraph.addEdge(4, 0);
     myGraph.addEdge(4, 2);
     myGraph.addEdge(5, 1);
     myGraph.addEdge(5, 0);
     myGraph.addEdge(0, 2);
-    myGraph.addEdge(2,3);
+
     int * distance = myGraph.BFS(myGraph, 5);
-    for(int i = 0; i < myGraph.size(); i++){
+    for(int i = 0; i < 100; i++){
         std::cout << distance[i] << std::endl;
     }
+
     Set<int> mySet(1000);
-    mySet = myGraph.outConnections(4);
+    mySet = myGraph.outConnections(0);
     mySet.displaySet();
 
-    if(myGraph.hasEdge(0,5))
-        std::cout << "Yeah" << std::endl;
-    myGraph.removeEdge(0, 5);
-    if(myGraph.hasEdge(0,5))
-        std::cout << "Yeah2" << std::endl;
 
 
 }
--- Zestaw03/Graph.hpp
+++ Zestaw03/Graph.hpp
@@ -1,9 +1,9 @@
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
+#include <vector>
 #include "Set.hpp"
 #include "LinkedList.hpp"
 #include "LinkedQueue.hpp"
-#include <queue>
 
 #define DEF_CAP 100
 
@@ -11,8 +11,6 @@ class Graph{
     private:
         LinkedList<int> * arr;
         int capacity;
-        int _size;
-        void resize(int newCapacity);
     public:
         Graph();
         Graph(int vertNum);
@@ -21,24 +19,12 @@ class Graph{
         void addEdge(int i, int j);
         void removeEdge(int i, int j);
         bool hasEdge(int i, int j);
-        int size() const;
         Set<int> inConnections(int i) const;
         Set<int> outConnections(int i) const;
         Set<int> allConnections(int i) const;
         int* BFS(const Graph& g, int s);
 };
 
-void Graph::resize(int newCapacity){
-    capacity = newCapacity;
-    LinkedList<int> * newArr;
-    newArr = new LinkedList<int>[newCapacity];
-    for(int i = 0; i < _size; i++){
-        newArr[i] = arr[i];
-    }
-    delete[] arr;
-    arr = newArr;
-
-}
 
 Graph::Graph(){
     capacity = DEF_CAP;
@@ -59,39 +45,21 @@ Graph::Graph(const Graph& other){
 }
 
 Graph::~Graph(){
-    delete[] arr;
+    delete arr;
 }
 
 void Graph::addEdge(int i, int j){
-    if(arr[i].empty())
-        _size++;
-    if(arr[j].empty())
-        _size++;
-    if(_size >= capacity){
-        resize(capacity*2);
-    }
     arr[i].push_back(j);
-    arr[j].push_back(i);
 }
 
 void Graph::removeEdge(int i, int j){
     int k = arr[i].remove(j);
-    k = arr[j].remove(i);
-    if(arr[i].empty())
-        _size--;
-    if(arr[j].empty())
-        _size--;
-    if(_size <= capacity/4)
-        resize(capacity/2);
 }
 
 bool Graph::hasEdge(int i, int j){
     return (arr[i].find(j) != -1);
 }
 
-int Graph::size() const {
-    return _size;
-}
 
 Set<int> Graph::inConnections(int i) const { //wszystkie wierzchołki od których wychodzą krawędzie do i
     Set<int> outSet(capacity);
@@ -131,14 +99,12 @@ Set<int> Graph::allConnections(int i) const { //suma dwóch poprzednich;
 }
 
 int* Graph::BFS(const Graph& g, int s){
-    int gSize = g.size();
     //std::queue<int> q;
     LinkedQueue q;
-    bool* visited = new bool[gSize];
-    int* paths = new int[gSize];
-
+    bool* visited = new bool[capacity];
+    int* paths = new int[capacity];
     
-    for(int i = 0; i < gSize; i++){
+    for(int i = 0; i < capacity; i++){
         visited[i] = false;
         paths[i] = -1;
     }
